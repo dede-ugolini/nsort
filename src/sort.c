5ced2e5efeb91e74be8d133c078022c26e6cccbe
Andre Ugolini <andreugolini@unochapeco.edu.br>
2026-03-08 13:11:57 -0300

    Selection and bubble sort.

---
+++ src/sort.c
@@ -0,0 +1,92 @@
+#include "sort.h"
+#include "nsort.h"
+
+#include <ncurses.h>
+#include <time.h>
+#include <unistd.h>
+
+Metrics selection_sort(Column columns[], int size, int y, int fps) {
+  struct timespec start, end;
+  clock_gettime(CLOCK_MONOTONIC, &start);
+  long long comparasions = 0;
+  long long swaps = 0;
+  Column min;
+  int minIndex;
+  int interval = SECOND / fps;
+  for (int i = 0; i < size - 1; i++) {
+    min = columns[i];
+    minIndex = i;
+    for (int j = i + 1; j < size; j++) {
+      columns[j].color = YELLOW;
+      columns[j].draw(columns[j], y, j);
+      columns[minIndex].color = RED;
+      columns[minIndex].draw(columns[minIndex], y, minIndex);
+      mvprintw(0, 0, "Selection Sort");
+      refresh();
+      usleep(interval);
+      columns[j].color = WHITE;
+      columns[j].draw(columns[j], y, j);
+      if (columns[j].height < min.height) {
+        columns[minIndex].color = WHITE;
+        columns[minIndex].draw(columns[minIndex], y, minIndex);
+        min = columns[j];
+        minIndex = j;
+        ++comparasions;
+      }
+    }
+
+    swap_columns(&columns[i], &columns[minIndex]);
+    ++swaps;
+
+    columns[minIndex].color = WHITE;
+    columns[minIndex].draw(columns[minIndex], y, minIndex);
+    columns[i].color = GREEN;
+    columns[i].draw(columns[i], y, i);
+  }
+  columns[size - 1].color = GREEN;
+  columns[size - 1].draw(columns[size - 1], y, size - 1);
+  clock_gettime(CLOCK_MONOTONIC, &end);
+  double elapsed =
+      (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+  Metrics metrics = {
+      comparasions,
+      swaps,
+      elapsed,
+  };
+  return metrics;
+}
+
+Metrics bubble_sort(Column columns[], int size, int y, int fps) {
+  struct timespec start, end;
+  clock_gettime(CLOCK_MONOTONIC, &start);
+  long long comparasions = 0;
+  long long swaps = 0;
+  int interval = SECOND / fps;
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size - i - 1; j++) {
+      columns[j].color = GREEN;
+      columns[j].draw(columns[j], y, j);
+      refresh();
+      usleep(interval);
+      if (columns[j].height > columns[j + 1].height) {
+        swap_columns(&columns[j], &columns[j + 1]);
+        ++swaps;
+      }
+      ++comparasions;
+      columns[j].color = WHITE;
+      columns[j].draw(columns[j], y, j);
+      refresh();
+    }
+    columns[size - i - 1].color = RED;
+    columns[size - i - 1].draw(columns[size - i - 1], y, size - i - 1);
+  }
+  clock_gettime(CLOCK_MONOTONIC, &end);
+  double elapsed =
+      (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+  Metrics metrics = {
+      comparasions,
+      swaps,
+      elapsed,
+  };
+  return metrics;
+}
---
+++ src/sort.h
@@ -0,0 +1,10 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include "nsort.h"
+
+Metrics selection_sort(Column columns[], int size, int y, int fps);
+
+Metrics bubble_sort(Column columns[], int size, int y, int fps);
+
+#endif
